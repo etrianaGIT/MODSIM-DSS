555fb3a12a3641c4286a44a9a1eeae4e005be1ff
Triana, Enrique <etriana@rti.org>
2026-09-18 12:14:51 -0500

    Adding a command line project for .NET8.0 with the windows projects referenced and the basic code to run MODSIM with the first argument.

---
+++ MainMODSIMLinux/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Csu.Modsim.ModsimIO;
+using Csu.Modsim.ModsimModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Hello, World!");
+// Program.cs
+
+string fileName = args[0];
+
+Model myModel = new Model();
+myModel.OnMessage += OnMessage;
+
+void OnMessage(string message)
+{
+    Console.WriteLine(message);
+}
+
+myModel.OnModsimError += OnError;
+
+void OnError(string message)
+{
+    Console.WriteLine(message);
+}
+
+XYFileReader.Read(myModel, fileName);
+
+// Adding plug-ins
+
+// foreach (Node res in myModel.Nodes_Reservoirs)
+//     res.m.min_volume = res.m.min_volume;
+
+Modsim.RunSolver(myModel);
+
+Console.ReadLine();
